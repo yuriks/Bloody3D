06a94fedc54aa47cc892bc7d5de8155f83d2c43f
yuriks <yuriks.br@gmail.com>
2010-10-09 15:20:08 -0300

    Matrix is now able to have height != width

--- Matrix.h
+++ Matrix.h
@@ -1,23 +1,23 @@
 #ifndef BLOODY3D_MATRIX
 #define BLOODY3D_MATRIX
 
-template <int N>
+template <int N, int M = N>
 struct mat
 {
 	void operator+=(const mat& m)
 	{
-		for (int i = 0; i < N*N; ++i) {
+		for (int i = 0; i < N*M; ++i) {
 			data[i] += m.data[i];
 		}
 	}
 
 	void operator*=(float f) {
-		for (int i = 0; i < N*N; ++i) {
+		for (int i = 0; i < N*M; ++i) {
 			data[i] *= f;
 		}
 	}
 
-	float data[N*N];
+	float data[N*M];
 };
 
 #endif // BLOODY3D_MATRIX
