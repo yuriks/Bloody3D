fd57d140fde526ff34acbeb7ac2fbbca502086e9
yuriks <yuriks.br@gmail.com>
2010-10-16 19:00:06 -0300

    Small matrix and vectors test on main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
+#include "Matrix.h"
+#include "Vector.h"
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
+	vec4 a = {4, 3, 2, 1};
+	mat4 b = {1, 0, 0, 2,
+	          0, 1, 0, 2,
+	          0, 0, 1, 0,
+	          0, 0, 0, 1};
+
+	vec4 r = b * a;
+	for (int i = 0; i < 4; ++i)
+		std::cout << r[i] << " ";
+
+	std::cin.get();
+
 	return 0;
 }
