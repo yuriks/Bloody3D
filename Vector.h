330caaf86eeefe65d6eb8d0da6ce5d652c86ff42
yuriks <yuriks.br@gmail.com>
2010-10-20 02:27:23 -0200

    Added missing 'inline' keyword to cross(vec3, vec3)

--- Vector.h
+++ Vector.h
@@ -19,7 +19,7 @@ float dot(const mat<N, 1>& v1, const mat<N, 1>& v2)
 	return r;
 }
 
-vec3 cross(const vec3& a, const vec3& b)
+inline vec3 cross(const vec3& a, const vec3& b)
 {
 	vec3 r = {a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1] - a[1]*b[0]};
 	return r;
