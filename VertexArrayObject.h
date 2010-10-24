ad16f167a20b6d40efeb54d9d630e9db6d0635a3
yuriks <yuriks.br@gmail.com>
2010-10-24 16:52:42 -0200

    Added gl::VertexArrayObject

---
+++ VertexArrayObject.h
@@ -0,0 +1,46 @@
+#ifndef BLOODY3D_GL_VERTEXARRAYOBJECT
+#define BLOODY3D_GL_VERTEXARRAYOBJECT
+
+#include "gl3w.h"
+
+#include <boost/noncopyable.hpp>
+
+namespace gl {
+
+class VertexArrayObject : boost::noncopyable
+{
+public:
+	VertexArrayObject();
+	~VertexArrayObject();
+
+	void bind() const;
+
+	operator GLuint() const;
+
+private:
+	GLuint id;
+};
+
+inline VertexArrayObject::VertexArrayObject() 
+{
+	glGenVertexArrays(1, &id);
+}
+
+inline VertexArrayObject::~VertexArrayObject()
+{
+	glDeleteVertexArrays(1, &id);
+}
+
+inline void VertexArrayObject::bind() const
+{
+	glBindVertexArray(id);
+}
+
+inline VertexArrayObject::operator GLuint() const
+{
+	return id;
+}
+
+} // namespace gl
+
+#endif // BLOODY3D_GL_VERTEXARRAYOBJECT
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Matrix.h"
 #include "Vector.h"
 #include "MatrixTransform.h"
+#include "VertexArrayObject.h"
+
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -74,79 +76,79 @@ int main(int argc, char *argv[])
 		glDebugMessageCallbackARB(debug_callback, 0);
 	}
 
-	GLuint vao_id;
-	glGenVertexArrays(1, &vao_id);
-	glBindVertexArray(vao_id);
+	{
+		gl::VertexArrayObject vao;
+		vao.bind();
 
-	GLuint vbo_id;
-	glGenBuffers(1, &vbo_id);
+		GLuint vbo_id;
+		glGenBuffers(1, &vbo_id);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data)*3, static_cast<const void*>(&data), GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data)*3, static_cast<const void*>(&data), GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 8*sizeof(float), (float*)0 + 0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_TRUE,  8*sizeof(float), (float*)0 + 4);
-	glEnableVertexAttribArray(1);
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 8*sizeof(float), (float*)0 + 0);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_TRUE,  8*sizeof(float), (float*)0 + 4);
+		glEnableVertexAttribArray(1);
 
-	GLuint vert_shader_id, frag_shader_id;
-	vert_shader_id = glCreateShader(GL_VERTEX_SHADER);
-	frag_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+		GLuint vert_shader_id, frag_shader_id;
+		vert_shader_id = glCreateShader(GL_VERTEX_SHADER);
+		frag_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
 
-	glShaderSource(vert_shader_id, 1, &vert_shader_src, 0);
-	glShaderSource(frag_shader_id, 1, &frag_shader_src, 0);
+		glShaderSource(vert_shader_id, 1, &vert_shader_src, 0);
+		glShaderSource(frag_shader_id, 1, &frag_shader_src, 0);
 
-	glCompileShader(vert_shader_id);
-	glCompileShader(frag_shader_id);
+		glCompileShader(vert_shader_id);
+		glCompileShader(frag_shader_id);
 
-	GLint vert_log_size, frag_log_size;
-	glGetShaderiv(vert_shader_id, GL_INFO_LOG_LENGTH, &vert_log_size);
-	glGetShaderiv(frag_shader_id, GL_INFO_LOG_LENGTH, &frag_log_size);
-	std::vector<char> log_buf(std::max(vert_log_size, frag_log_size));
+		GLint vert_log_size, frag_log_size;
+		glGetShaderiv(vert_shader_id, GL_INFO_LOG_LENGTH, &vert_log_size);
+		glGetShaderiv(frag_shader_id, GL_INFO_LOG_LENGTH, &frag_log_size);
+		std::vector<char> log_buf(std::max(vert_log_size, frag_log_size));
 
-	glGetShaderInfoLog(vert_shader_id, log_buf.size(), 0, &log_buf[0]);
-	std::cout << &log_buf[0];
-	glGetShaderInfoLog(frag_shader_id, log_buf.size(), 0, &log_buf[0]);
-	std::cout << &log_buf[0];
+		glGetShaderInfoLog(vert_shader_id, log_buf.size(), 0, &log_buf[0]);
+		std::cout << &log_buf[0];
+		glGetShaderInfoLog(frag_shader_id, log_buf.size(), 0, &log_buf[0]);
+		std::cout << &log_buf[0];
 
 
-	GLuint shader_prog_id;
-	shader_prog_id = glCreateProgram();
+		GLuint shader_prog_id;
+		shader_prog_id = glCreateProgram();
 
-	glAttachShader(shader_prog_id, vert_shader_id);
-	glAttachShader(shader_prog_id, frag_shader_id);
+		glAttachShader(shader_prog_id, vert_shader_id);
+		glAttachShader(shader_prog_id, frag_shader_id);
 
-	glBindAttribLocation(shader_prog_id, 0, "in_Position");
-	glBindAttribLocation(shader_prog_id, 1, "in_Color");
+		glBindAttribLocation(shader_prog_id, 0, "in_Position");
+		glBindAttribLocation(shader_prog_id, 1, "in_Color");
 
-	glLinkProgram(shader_prog_id);
+		glLinkProgram(shader_prog_id);
 
-	glDeleteShader(vert_shader_id);
-	glDeleteShader(frag_shader_id);
+		glDeleteShader(vert_shader_id);
+		glDeleteShader(frag_shader_id);
 
-	glUseProgram(shader_prog_id);
+		glUseProgram(shader_prog_id);
 
-	bool running = true;
+		bool running = true;
 
-	vec3 s = {600./800., 1.f, 1.f};
-	mat4 m = mat_transform::scale(s);
+		vec3 s = {600./800., 1.f, 1.f};
+		mat4 m = mat_transform::scale(s);
 
-	while (running) {
-		glClearColor(0.f, 0.f, 0.f, 1.f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		while (running) {
+			glClearColor(0.f, 0.f, 0.f, 1.f);
+			glClear(GL_COLOR_BUFFER_BIT);
 
-		glUniformMatrix4fv(glGetUniformLocation(shader_prog_id, "in_Proj"), 1, false, &m.data[0]);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+			glUniformMatrix4fv(glGetUniformLocation(shader_prog_id, "in_Proj"), 1, false, &m.data[0]);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		glfwSwapBuffers();
+			glfwSwapBuffers();
 
-		running = glfwGetWindowParam(GLFW_OPENED) != 0;
+			running = glfwGetWindowParam(GLFW_OPENED) != 0;
+		}
+
+		glDeleteProgram(shader_prog_id);
+
+		glDeleteBuffers(1, &vbo_id);
 	}
-
-	glDeleteProgram(shader_prog_id);
-
-	glDeleteBuffers(1, &vbo_id);
-	glDeleteVertexArrays(1, &vao_id);
 
 	glfwTerminate();
 
