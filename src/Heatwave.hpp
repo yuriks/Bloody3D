89712feaba4f9c8fc97145244b89b06ccd092b58
yuriks <yuriks@yuriks.net>
2011-10-20 18:55:47 -0200

    Moved sized type typedefs out of hw namespace.
    I don't think I'll be having any conflicts with those, and putting them
    inside hw is too verbose.

--- src/Heatwave.hpp
+++ src/Heatwave.hpp
@@ -17,14 +17,12 @@
 #endif
 #define HW_ALIGN_VAR_SSE HW_ALIGN_VAR(16)
 
-namespace hw {
-	// Standard sized types
-	typedef int8_t s8;
-	typedef uint8_t u8;
-	typedef int16_t s16;
-	typedef uint16_t u16;
-	typedef int32_t s32;
-	typedef uint32_t u32;
-	typedef int64_t s64;
-	typedef uint64_t u64;
-}
+// Standard sized types
+typedef int8_t s8;
+typedef uint8_t u8;
+typedef int16_t s16;
+typedef uint16_t u16;
+typedef int32_t s32;
+typedef uint32_t u32;
+typedef int64_t s64;
+typedef uint64_t u64;
