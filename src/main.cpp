52af9fe4ed007222321dbb9e51faefe70fb26581
yuriks <yuriks@yuriks.net>
2011-10-20 19:59:24 -0200

    Updated example to use GPUMesh.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "math/MatrixTransform.hpp"
 #include "math/Quaternion.hpp"
 #include "mesh/Material.hpp"
+#include "mesh/VertexFormats.hpp"
+#include "mesh/GPUMesh.hpp"
 
 #include <iostream>
 #include <fstream>
@@ -77,42 +79,23 @@ bool init_window()
 	return true;
 }
 
-HW_ALIGN_VAR_SSE const float triangle_vertex_data[3*4] = {
-	 0.f,   0.5f, 0.f, 0.f,
-	 0.5f, -0.5f, 0.f, 0.f,
-	-0.5f, -0.5f, 0.f, 0.f
+HW_ALIGN_VAR_SSE static const vertex_fmt::Pos3f triangle_vertex_data[3] = {
+	{ math::vec3(0.f, 0.5f, 0.f) },
+	{ math::vec3(0.5f, -0.5f, 0.f) },
+	{ math::vec3(-0.5f, -0.5f, 0.f) }
 };
+
+static const u16 triangle_indices[3] = { 0, 1, 2 };
 
 int main(int argc, char *argv[])
 {
 	if (!init_window())
 		return 1;
 
-	struct MeshGLState
 	{
-		gl::VertexArrayObject vao;
-		gl::BufferObject vbo;
-		gl::BufferObject ibo;
-	};
-
-	{
-		MeshGLState mesh_state;
-
-		MeshGLState& s = mesh_state;
-
-		s.vao.bind();
-
-		s.vbo.bind(GL_ARRAY_BUFFER);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(float)*3*4, triangle_vertex_data, GL_STATIC_DRAW);
-
-		// 0 - in_Position
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*4, (char*)0);
-		glEnableVertexAttribArray(0);
-		//glVertexAttribPointer(1, 2, GL_FLOAT, GL_TRUE, sizeof(Vertex), (char*)0 + offsetof(Vertex, texcoord));
-		//glEnableVertexAttribArray(1);
-
-		//s.ibo.bind(GL_ELEMENT_ARRAY_BUFFER);
-		//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * indices.size(), static_cast<const void*>(indices.data()), GL_STATIC_DRAW);
+		GPUMesh mesh;
+		mesh.loadVertexData(triangle_vertex_data, sizeof(triangle_vertex_data), vertex_fmt::FMT_POS3F);
+		mesh.loadIndices(triangle_indices, 3);
 
 		{
 			Material material;
@@ -263,9 +246,9 @@ int main(int argc, char *argv[])
 
 				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-				mesh_state.vao.bind();
-				glDrawArrays(GL_TRIANGLES, 0, 3);
-				//glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, (char*)0);
+				mesh.vao.bind();
+				mesh.ibo.bind(GL_ELEMENT_ARRAY_BUFFER);
+				glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, 0);
 
 				glfwSwapBuffers();
 
