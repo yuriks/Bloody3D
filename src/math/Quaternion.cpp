5593df60b1fe2672137e2b9abaa55589826f80e9
yuriks <yuriks@yuriks.net>
2011-10-21 20:19:27 -0200

    Moved some quaternion functions to cpp and correctly marked the rest inline.

---
+++ src/math/Quaternion.cpp
@@ -0,0 +1,19 @@
+#include "Quaternion.hpp"
+
+namespace math {
+
+mat3x4 matrixFromQuaternion(const Quaternion& q) {
+	const float x = q.val.getX();
+	const float y = q.val.getY();
+	const float z = q.val.getZ();
+	const float w = q.val.getW();
+
+	mat3x4 m;
+	m.rows[0] = vec4(1.f - 2.f*y*y - 2.f*z*z,       2.f*x*y - 2.f*z*w,       2.f*x*z + 2.f*y*w, 0.f);
+	m.rows[1] = vec4(      2.f*x*y + 2.f*z*w, 1.f - 2.f*x*x - 2.f*z*z,       2.f*y*z - 2.f*x*w, 0.f);
+	m.rows[2] = vec4(      2.f*x*z - 2.f*y*w,       2.f*y*z + 2.f*x*w, 1.f - 2.f*x*x - 2.f*y*y, 0.f);
+
+	return m;
+}
+
+} // namespace math
--- src/math/Quaternion.hpp
+++ src/math/Quaternion.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Vector.hpp"
+#include "Matrix.hpp"
 
 #include <cmath>
 
@@ -20,7 +21,7 @@ struct Quaternion {
 	}
 };
 
-Quaternion operator*(const Quaternion& p, const Quaternion& q) {
+inline Quaternion operator*(const Quaternion& p, const Quaternion& q) {
 	const vec3 pv = vec3(p.val);
 	const vec3 qv = vec3(q.val);
 	vec4 val = vec4((vec3(p.val.spreadW().xmm) * qv) + (vec3(q.val.spreadW().xmm) * pv) + cross(pv, qv));
@@ -28,26 +29,14 @@ Quaternion operator*(const Quaternion& p, const Quaternion& q) {
 	return Quaternion(val);
 }
 
-Quaternion conjugate(const Quaternion& q) {
+inline Quaternion conjugate(const Quaternion& q) {
 	return Quaternion(vec4(_mm_xor_ps(q.val.xmm, _mm_setr_ps(-0.f, -0.f, -0.f, 0.f))));
 }
 
-vec3 rotate(const Quaternion& q, const vec3& v) {
+inline vec3 rotate(const Quaternion& q, const vec3& v) {
 	return vec3((q * Quaternion(vec4(v)) * conjugate(q)).val);
 }
 
-mat3x4 matrixFromQuaternion(const Quaternion& q) {
-	const float x = q.val.getX();
-	const float y = q.val.getY();
-	const float z = q.val.getZ();
-	const float w = q.val.getW();
-
-	mat3x4 m;
-	m.rows[0] = vec4(1.f - 2.f*y*y - 2.f*z*z,       2.f*x*y - 2.f*z*w,       2.f*x*z + 2.f*y*w, 0.f);
-	m.rows[1] = vec4(      2.f*x*y + 2.f*z*w, 1.f - 2.f*x*x - 2.f*z*z,       2.f*y*z - 2.f*x*w, 0.f);
-	m.rows[2] = vec4(      2.f*x*z - 2.f*y*w,       2.f*y*z + 2.f*x*w, 1.f - 2.f*x*x - 2.f*y*y, 0.f);
-
-	return m;
-}
+mat3x4 matrixFromQuaternion(const Quaternion& q);
 
 } // namespace math
