3cf98bc02b590b22de6b04c593c6b94648193683
yuriks <yuriks@yuriks.net>
2011-10-22 00:33:16 -0200

    Added spherical map normal encoding and decoding.

--- data/shaders/common.glsl
+++ data/shaders/common.glsl
@@ -19,6 +19,18 @@ vec4 transform(mat3x4_rows m, vec4 v) {
 	return vec4(dot(m.rows[0], v), dot(m.rows[1], v), dot(m.rows[2], v), v.w);
 }
 
+vec2 encodeNormal(vec3 n) {
+	vec2 enc = normalize(n.xy) * sqrt(-n.z);
+	return enc;
+}
+
+vec3 decodeNormal(vec2 enc) {
+	vec3 n;
+	n.z = -dot(enc, enc);
+	n.xy = normalize(enc) * sqrt(1 - n.z*n.z);
+	return n;
+}
+
 struct VertFragData {
 	vec3 normal;
 	vec2 tex_coord;
