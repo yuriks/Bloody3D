4276a8817d4f69815ed772ab66da6d6e5702988d
yuriks <yuriks@yuriks.net>
2011-11-02 03:14:33 -0200

    Gamma correct rendering.
    Enabled sRGB framebuffer blending and added the sRGB flag to the texture.

--- src/main.cpp
+++ src/main.cpp
@@ -126,7 +126,7 @@ int main(int argc, char *argv[])
 				MaterialOptions mtl_options;
 				mtl_options.uniforms = std::move(u);
 				mtl_options.texture_ids.fill(-1);
-				mtl_options.texture_ids[0] = scene.tex_manager.loadTexture("panel_beams_diffuse.png");
+				mtl_options.texture_ids[0] = scene.tex_manager.loadTexture("panel_beams_diffuse.png", TEXF_SRGB);
 				mtl_options.texture_ids[1] = scene.tex_manager.loadTexture("panel_beams_normal.png");
 				mesh.material_options = mtl_options;
 			}
@@ -159,6 +159,7 @@ int main(int argc, char *argv[])
 			glEnable(GL_CULL_FACE);
 			//glEnable(GL_DEPTH_CLAMP);
 			glFrontFace(GL_CW);
+			glEnable(GL_FRAMEBUFFER_SRGB);
 			//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 			scene::RenderBufferSet def_buffers;
