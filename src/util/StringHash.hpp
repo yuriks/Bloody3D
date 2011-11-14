33f846c4b258b1613856280c154206cee78d1167
yuriks <yuriks@yuriks.net>
2011-11-14 07:30:57 -0200

    Fixed string hashing functions.
    Previously the trailing null in the string wasn't being accounted in
    fnv_hash_runtime.
    Also removed a redundant function.

--- src/util/StringHash.hpp
+++ src/util/StringHash.hpp
@@ -24,41 +24,35 @@ HW_FORCE_INLINE u32 fnv_hash<N, 1>(const char (&str)[N])
 */
 
 template <unsigned int N>
-HW_FORCE_INLINE u32 fnv_hash_template(const char (&str)[N])
+HW_FORCE_INLINE u32 fnv_hash(const char (&str)[N])
 {
-	return (((((fnv_hash_template<N-3>(str) ^ str[N-3]) * 16777619u) ^ str[N-2]) * 16777619u) ^ str[N-1]) * 16777619u;
+	return (((((fnv_hash<N-3>((const char (&)[N-3])str) ^ str[N-3]) * 16777619u) ^ str[N-2]) * 16777619u) ^ str[N-1]) * 16777619u;
 }
 
 template<>
-HW_FORCE_INLINE u32 fnv_hash_template<3>(const char (&str)[3])
+HW_FORCE_INLINE u32 fnv_hash<3>(const char (&str)[3])
 {
 	return (((((2166136261u ^ str[0]) * 16777619u) ^ str[1]) * 16777619u) ^ str[2]) * 16777619u;
 }
 
 template<>
-HW_FORCE_INLINE u32 fnv_hash_template<2>(const char (&str)[2])
+HW_FORCE_INLINE u32 fnv_hash<2>(const char (&str)[2])
 {
 	return (((2166136261u ^ str[0]) * 16777619u) ^ str[1]) * 16777619u;
 }
 
 template<>
-HW_FORCE_INLINE u32 fnv_hash_template<1>(const char (&str)[1])
+HW_FORCE_INLINE u32 fnv_hash<1>(const char (&str)[1])
 {
 	return (2166136261u ^ str[0]) * 16777619u;
-}
-
-template <unsigned int N>
-HW_FORCE_INLINE u32 fnv_hash(const char (&str)[N])
-{
-	return fnv_hash_template<N, N>(str);
 }
 
 inline u32 fnv_hash_runtime(const char *str)
 {
 	u32 hash = 2166136261u;
-	while (*str != '\0') {
-		hash = (hash ^ *str++) * 16777619u;
-	}
+	do {
+		hash = (hash ^ *str) * 16777619u;
+	} while (*str++ != '\0');
 	return hash;
 }
 
