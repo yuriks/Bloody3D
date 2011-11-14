b12ffd041abd6e2e3a5347dbb05641fb1aedfd0c
yuriks <yuriks@yuriks.net>
2011-11-14 04:31:18 -0200

    Added compile and runtime string hashing using the FNV-1a hash.

---
+++ src/util/StringHash.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "Heatwave.hpp"
+
+namespace util {
+
+// Implements a compile-time FNV-1a string hash
+// Thanks to http://altdevblogaday.com/2011/10/27/quasi-compile-time-string-hashing/
+
+/*
+
+template <unsigned int N, unsigned int I>
+HW_FORCE_INLINE u32 fnv_hash(const char (&str)[N])
+{
+	return (fnv_hash<N, I-1>(str) ^ str[I-1]) * 16777619u;
+}
+
+template <unsigned int N>
+HW_FORCE_INLINE u32 fnv_hash<N, 1>(const char (&str)[N])
+{
+	return (2166136261u ^ str[0]) * 16777619u;
+}
+
+*/
+
+template <unsigned int N>
+HW_FORCE_INLINE u32 fnv_hash_template(const char (&str)[N])
+{
+	return (((((fnv_hash_template<N-3>(str) ^ str[N-3]) * 16777619u) ^ str[N-2]) * 16777619u) ^ str[N-1]) * 16777619u;
+}
+
+template<>
+HW_FORCE_INLINE u32 fnv_hash_template<3>(const char (&str)[3])
+{
+	return (((((2166136261u ^ str[0]) * 16777619u) ^ str[1]) * 16777619u) ^ str[2]) * 16777619u;
+}
+
+template<>
+HW_FORCE_INLINE u32 fnv_hash_template<2>(const char (&str)[2])
+{
+	return (((2166136261u ^ str[0]) * 16777619u) ^ str[1]) * 16777619u;
+}
+
+template<>
+HW_FORCE_INLINE u32 fnv_hash_template<1>(const char (&str)[1])
+{
+	return (2166136261u ^ str[0]) * 16777619u;
+}
+
+template <unsigned int N>
+HW_FORCE_INLINE u32 fnv_hash(const char (&str)[N])
+{
+	return fnv_hash_template<N, N>(str);
+}
+
+inline u32 fnv_hash_runtime(const char *str)
+{
+	u32 hash = 2166136261u;
+	while (*str != '\0') {
+		hash = (hash ^ *str++) * 16777619u;
+	}
+	return hash;
+}
+
+} // namespace util
