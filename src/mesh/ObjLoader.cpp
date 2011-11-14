yuriks/Bloody3D

commit 66271e23dba72ef44693a96271279a2337f41b1e

Changed ObjLoader so that it correctly loads files with missing vertex attribs.

src/mesh/ObjLoader.cpp
@@ -14,7 +14,7 @@ using std::get;
 
 Mesh load_obj(std::istream& f)
 {
-	typedef std::tuple<unsigned int, unsigned int, unsigned int> Triangle;
+	typedef std::tuple<int, int, int> Triangle;
 	typedef std::tuple<float, float> floatPair;
 
 	Mesh mesh;
@@ -73,15 +73,33 @@ Mesh load_obj(std::istream& f)
 				{
 					std::string tri_str;
 					ss >> tri_str;
-					// Replace all '/'s with ' '
-					for (unsigned int p = tri_str.find('/'); p != std::string::npos; p = tri_str.find('/', p + 1))
-					{
-						tri_str[p] = ' ';
-					}
 
-					std::istringstream fss(tri_str);
 					Triangle tri;
-					fss >> get<0>(tri) >> get<1>(tri) >> get<2>(tri);
+
+					if (tri_str[0] == '/') {
+						get<0>(tri) = -1;
+						tri_str.erase(0, 1);
+					} else {
+						size_t off;
+						get<0>(tri) = std::stoi(tri_str, &off);
+						tri_str.erase(0, off+1);
+					}
+					if (tri_str[0] == '/') {
+						get<1>(tri) = -1;
+						tri_str.erase(0, 1);
+					} else {
+						size_t off;
+						get<1>(tri) = std::stoi(tri_str, &off);
+						tri_str.erase(0, off+1);
+					}
+					if (tri_str.empty()) {
+						get<2>(tri) = -1;
+						tri_str.erase(0, 1);
+					} else {
+						size_t off;
+						get<2>(tri) = std::stoi(tri_str, &off);
+						tri_str.erase(0, off);
+					}
 
 					// Try to find key in map
 					auto lb = vertex_cache.lower_bound(tri);
@@ -94,17 +112,17 @@ Mesh load_obj(std::istream& f)
 					{
 						// Doesn't exist yet
 						vertex_fmt::Pos3f_Norm3f_Tex2f vert;
-						unsigned int pos_i = get<0>(tri)-1;
-						unsigned int uv_i = get<1>(tri)-1;
-						unsigned int norm_i = get<2>(tri)-1;
+						int pos_i = get<0>(tri);
+						int uv_i = get<1>(tri);
+						int norm_i = get<2>(tri);
 
-						if (pos_i >= pos_db.size() || norm_i >= norm_db.size() || uv_i >= uv_db.size()) {
+						if ((pos_i != -1 && (unsigned)pos_i-1 >= pos_db.size()) || (norm_i != -1 && (unsigned)norm_i-1 >= norm_db.size()) || (uv_i != -1 && (unsigned)uv_i >= uv_db.size())) {
 							std::cerr << "Invalid vertex index: " << pos_i << ' ' << uv_i << ' ' << norm_i << std::endl;
 							goto abort_while;
 						}
-						vert.pos = pos_db[pos_i];
-						vert.norm = norm_db[norm_i];
-						auto pair = uv_db[uv_i];
+						vert.pos = pos_i == -1 ? vec3(0.f) : pos_db[pos_i-1];
+						vert.norm = norm_i == -1 ? vec3(0.f) : norm_db[norm_i-1];
+						auto pair = uv_i == -1 ? floatPair(0.f, 0.f) : uv_db[uv_i-1];
 						vert.tex_coord[0] = get<0>(pair);
 						vert.tex_coord[1] = get<1>(pair);
 

commit 696186bba130a190b707b7bbe638fd1765cf1eaf

Fixed bug in ObjLoader that was causing it to ignore last vertices in the mesh.
Bug was introduced in 66271e23dba72ef44693a96271279a2337f41b1e.

src/mesh/ObjLoader.cpp
@@ -116,7 +116,7 @@ Mesh load_obj(std::istream& f)
 						int uv_i = get<1>(tri);
 						int norm_i = get<2>(tri);
 
-						if ((pos_i != -1 && (unsigned)pos_i-1 >= pos_db.size()) || (norm_i != -1 && (unsigned)norm_i-1 >= norm_db.size()) || (uv_i != -1 && (unsigned)uv_i >= uv_db.size())) {
+						if ((pos_i != -1 && (unsigned)pos_i-1 >= pos_db.size()) || (norm_i != -1 && (unsigned)norm_i-1 >= norm_db.size()) || (uv_i != -1 && (unsigned)uv_i-1 >= uv_db.size())) {
 							std::cerr << "Invalid vertex index: " << pos_i << ' ' << uv_i << ' ' << norm_i << std::endl;
 							goto abort_while;
 						}