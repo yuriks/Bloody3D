488496ba08d26742639910c810e530833b8edd2a
yuriks <yuriks@yuriks.net>
2011-11-14 04:36:27 -0200

    Added HWMesh binary mesh file format structs.

---
+++ src/mesh/HWMesh.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Heatwave.hpp"
+#include "VertexFormats.hpp"
+
+namespace mesh {
+
+struct HWMeshHeader {
+	// Header magic
+	char magic[6]; // = "HWMESH"
+	// Version number,
+	u16 version; // = 0
+
+	u16 num_meshes;
+};
+static_assert(sizeof(HWMeshHeader) == 10, "Struct size changed! (Breaks I/O compatibility.)");
+
+struct HWMeshIndex {
+	u32 name_hash; // Hashed string identifier for mesh
+	u32 file_offset; // Offset into file where data begins
+};
+static_assert(sizeof(HWMeshIndex) == 8, "Struct size changed! (Breaks I/O compatibility.)");
+
+struct HWMeshData {
+	u8 index_type; // 1 = UNSIGNED_BYTE, 2 = UNSIGNED_SHORT
+	u8 vertex_format; // See VertexFormat enum in VertexFormats.hpp
+	u8 num_submeshes; // max 16
+
+	u8 reserved;
+
+	u16 submesh_indices_offset[16];
+	u16 submesh_indices_size[16];
+
+	u32 vertex_data_size; // in bytes
+	u32 index_data_size; // in bytes
+
+	//ALIGN(16);
+	//u8 vertex_data[vertex_data_size];
+
+	//ALIGN(4);
+	//u8 index_data[index_data_size];
+};
+static_assert(sizeof(HWMeshData) == 76, "Struct size changed! (Breaks I/O compatibility.)");
+
+} // namespace mesh
--- src/mesh/VertexFormats.hpp
+++ src/mesh/VertexFormats.hpp
@@ -14,10 +14,11 @@ struct Pos3f_Norm3f_Tex2f {
 	float tex_coord[2];
 };
 
+// DON'T CHANGE VALUES
 enum VertexFormat {
-	FMT_NONE,
-	FMT_POS3F,
-	FMT_POS3F_NORM3F_TEX2F
+	FMT_NONE = 0,
+	FMT_POS3F = 1,
+	FMT_POS3F_NORM3F_TEX2F = 2
 };
 
 } // namespaec vertex_formats
