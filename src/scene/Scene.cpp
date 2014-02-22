d566e2198626005dcb12841a2003d539da1fe6b8
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-02-22 16:22:36 -0300

    Explicitly set max level for framebuffer textures.
    Silences a driver warning in nVidia

--- src/scene/Scene.cpp
+++ src/scene/Scene.cpp
@@ -20,6 +20,7 @@ void createBuffers(int width, int height, const BufferSpec* specs, size_t spec_n
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
 		glTexImage2D(GL_TEXTURE_2D, 0, specs[i].internal_format, width, height, 0,
 			specs[i].format, specs[i].type, nullptr);
 	}
